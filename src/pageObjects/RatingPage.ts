2b380f660beba6b75954224ce3eacb8b4fc50806
hybridtheo7y <hybridtheo7y@gmail.com>
2021-08-05 23:49:36 +0300

    ДЗ №3 Ульянов - исправление замечаний

--- src/pageObjects/RatingPage.ts
+++ src/pageObjects/RatingPage.ts
@@ -4,6 +4,11 @@ import Page from './page';
  * sub page containing specific selectors and methods for a specific page
  */
 class RatingPage extends Page {
+
+  get LikesTable() {
+    return  browser.$$('//td[contains(@class,\'success\')]');
+  }
+
   waitForLoaded() {
     super.waitForLoaded();
     return browser.waitUntil(async () => {
--- test/specs/hw3.ts
+++ test/specs/hw3.ts
@@ -13,32 +13,22 @@ describe('Упорядоченность ретинга лайков', async () 
     const CatRatingBtn = await MainPage.CatRatingBtn;
     await CatRatingBtn.click();
     await RatingPage.waitForLoaded();
-    const LikesTable = await browser.$$('//td[contains(@class,\'success\')]');
-    const NamesTable = await browser.$$('//table[1]//td[contains(@class,\'1OPc8\')]'); // в данном случае не смог придумать селектора лучше, во второй таблице у элемента имени точно такие же данные
+    const LikesTable = await RatingPage.LikesTable;
+
 
     const LikesNumbersTable: string[] = [];
-    for (const tmp_like of LikesTable) {
-      const like = await tmp_like.getText();
-      LikesNumbersTable.push(like);
+    for (const TmpLike of LikesTable) {
+      const Like = await TmpLike.getText();
+      LikesNumbersTable.push(Like);
     }
 
-    const ResultNamesTable: string[] = [];
-    for (const tmp_name of NamesTable) {
-      const name = await tmp_name.getText();
-      ResultNamesTable.push(name);
-    }
-
-    const Num_of_Likes = LikesNumbersTable.map(i=>Number(i))
+    const ActualLikes = LikesNumbersTable.map(i=>Number(i));
+    const SortedLikes = ActualLikes.sort((a, b) => b - a);
 
     allureReporter.startStep('Проверка упорядоченности лайков');
-    for (let i = 0; i < Num_of_Likes.length - 1; i++){
-      expect(Num_of_Likes[i]).toBeGreaterThanOrEqual(Num_of_Likes[i+1]);
-      const msg = `Значение лайков котика ${ResultNamesTable[i]} №${i+1} рейтинга`;
-      allureReporter.addAttachment(msg, Num_of_Likes[i].toString(), 'text/plain')
-    }
-
-    const msg = `Значение лайков котика ${ResultNamesTable[Num_of_Likes.length - 1]} №${Num_of_Likes.length} рейтинга`;
-    allureReporter.addAttachment(msg, Num_of_Likes[Num_of_Likes.length - 1].toString(), 'text/plain')
+    allureReporter.addAttachment('Ожидаемый массив лайков', SortedLikes.join('; '), 'text/plain');
+    allureReporter.addAttachment('Актуальный массив лайков', ActualLikes.join('; '), 'text/plain');
+    expect(ActualLikes).toEqual(SortedLikes);
     allureReporter.endStep();
 
   });
